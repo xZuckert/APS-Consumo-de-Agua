822239b5d1439b7079a40fea0037dcbea2e891d5
villeskaique <villeskaique@gmail.com>
2025-04-16 04:50:42 -0300

    Adicionado metodo para posicionar o Toast

--- APSConsumoDeAgua/src/main/java/org/example/apsconsumodeagua/utils/Toast.java
+++ APSConsumoDeAgua/src/main/java/org/example/apsconsumodeagua/utils/Toast.java
@@ -33,6 +33,29 @@ public class Toast {
         timeline.setCycleCount(1);
         timeline.play();  // Inicia a animação
     }
+    public static void mostrarToast(AnchorPane root, String message, tipoToast tipo,int posX, int posY) {
+        String tipoal = switch (tipo) {
+            case ALERT -> "rgba(255,0,0,0.7);";
+            case SUCESSO -> "rgba(0,255,0,0.7);";
+            case ERRO -> "rgba(255,0,0.7);";
+        };
+
+        Label toast = new Label(message);
+        toast.setFont(new Font("Arial", 16));
+        toast.setTextFill(Color.WHITE);
+        toast.setStyle("-fx-background-color: " + tipoal + "; -fx-padding: 10px;");
+
+        AnchorPane.setLeftAnchor(toast, (double) posX);
+        AnchorPane.setBottomAnchor(toast, (double) posY);
+
+        root.getChildren().add(toast);
+
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(3), event -> root.getChildren().remove(toast))
+        );
+        timeline.setCycleCount(1);
+        timeline.play();  // Inicia a animação
+    }
 }
 
 
