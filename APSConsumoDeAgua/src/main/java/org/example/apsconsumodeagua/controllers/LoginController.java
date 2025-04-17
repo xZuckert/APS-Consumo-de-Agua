bb77b7cf9cffb8002a0d6d01ec868aa75c8588e2
villeskaique <villeskaique@gmail.com>
2025-04-17 19:20:19 -0300

    Trocado o nome da classe Login para LoginController para padronização

--- APSConsumoDeAgua/src/main/java/org/example/apsconsumodeagua/controllers/LoginController.java
+++ APSConsumoDeAgua/src/main/java/org/example/apsconsumodeagua/controllers/LoginController.java
@@ -6,18 +6,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import org.example.apsconsumodeagua.Application;
 
 import java.io.IOException;
-import java.net.PasswordAuthentication;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Login implements Initializable {
+public class LoginController implements Initializable {
     @FXML
     private AnchorPane paneInterface;
     @FXML
