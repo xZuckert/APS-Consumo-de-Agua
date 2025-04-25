065806ff2e11109c0857fd6b6dbfcc76d64464e8
xZuckert <xXPZCOCAXx@outlook.com>
2025-04-25 11:35:10 -0300

    Troca da conexão com o Banco de Dados com servidor local para um servidor nuvem com Railway

--- APSConsumoDeAgua/src/main/java/org/example/apsconsumodeagua/database/DatabaseConnection.java
+++ APSConsumoDeAgua/src/main/java/org/example/apsconsumodeagua/database/DatabaseConnection.java
@@ -7,7 +7,7 @@ public class DatabaseConnection {
     public static Connection conectarDB(Connection conexao) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/aps_consumo_de_agua?user=root&password="); // Cria a conexão
+            conexao = DriverManager.getConnection("jdbc:mysql://metro.proxy.rlwy.net:52382/railway?user=root&password=bWnNpXxdRFOOmaZPYTjSGgOuhDgSlACm"); // Cria a conexão
             return conexao;
         } catch (Exception e) {
             System.out.println("Erro ao conectar ao banco de dados " + e);
